4c6eeabc16b183c82bef8289da533c86facd5c57
Vladyslav Savchyn <57039458+Barero-dreep@users.noreply.github.com>
2023-12-22 15:00:17 -0500

    add: initial commit with linked and restructured file system

---
+++ Game/include/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// #include <stdio.h>
+#include <iostream>
+#include <tchar.h>
+#include <math.h>
+#include <string>
+
+#include "App\app.h"
+
+// TODO: reference additional headers your program requires here
